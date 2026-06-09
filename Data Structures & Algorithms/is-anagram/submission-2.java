85e4c12c9455c1e12dde3ce9ae4755aff57b8a26
Anand Karandikar <anandk10@gmail.com>
2026-06-08 17:54:11 -0700

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int[] freq = new int[26];
+
+        for (char c : s.toCharArray()) {
+            freq[c - 'a'] += 1;
+        }
+
+        for (char c : t.toCharArray()) {
+            freq[c - 'a'] -= 1;
+        }
+
+        for (int c : freq) {
+            if (c != 0) return false;
+        }
+        return true;
+    }
+}
